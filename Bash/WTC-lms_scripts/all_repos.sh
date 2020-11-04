3ca3ad9946dafaa6b2a60d744fda7c162d6e8ab8
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-04 09:54:07 +0200

    Creation of Folders 'Bash', 'Python', 'Ruby'

---
+++ Bash/WTC-lms_scripts/all_repos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+wtc-lms start 291fe834-5e48-4c8d-a8eb-5dfba8e3cd3a
+wtc-lms start 9e760d4f-adc7-4184-8c6c-538dd5c02532
+wtc-lms start aeda1496-2eeb-49ed-9c86-d84593456cc3
+wtc-lms start c4ed8104-22a0-4b2b-96a8-879b4c9e6e69
+wtc-lms start 0b447f96-8fb9-4876-ae91-046267423a3e
+wtc-lms start eb04c6dc-ddee-4751-a999-6e28f7861978 #mastermind
+wtc-lms start ef4e20e2-6ed5-4eea-93b6-78116b0083eb
+wtc-lms start ead9542e-a32a-43d2-bfcf-c60e7559611f
+wtc-lms start ec83fd21-6c13-4a78-9396-f57c8ffd3c83
+wtc-lms start decea39d-7a2a-45d9-bd08-1af152c94516
+wtc-lms start e5be25a3-5fd4-4f71-8dc0-67e3b8b211bf
+wtc-lms start 254a6e98-30b6-4a8b-b255-a4e7aa0c4547
+wtc-lms start 8dea9e69-07da-440d-964c-08e868e11561
+wtc-lms start da2f7a67-be13-4e03-995d-1c63dc429bd6
+wtc-lms start c96a3dc4-1ee0-4cac-8d78-11a63c8213fd
+wtc-lms start f899e822-161f-4c92-9a42-7c7a2467c021
