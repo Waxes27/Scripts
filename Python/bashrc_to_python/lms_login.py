57c278eb3bc605cc587ee68da25bb30ceb0ab2f1
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-05 17:30:13 +0200

    lms-login update

--- Python/bashrc_to_python/lms_login.py
+++ Python/bashrc_to_python/lms_login.py
@@ -2,12 +2,40 @@ import os
 import sys
 import time
 import subprocess
-os.system("pip3 install selenium")
-os.system('clear')
-import selenium
 
-# help('modules')
-list_of_commands = ['Help', 'Off', 'Home']
+
+list_of_commands = ['help', 'off', 'terminal (in dev)']
+lms_commands = ['Topics','Problems','Reviews']
+f_flags = ['fundamentals', 'object orientation', 'web application', 'Mobile Applications', 'Distributed Systems']
+p_flags = ['Hangman','Pyramid','Outline','Mastermind','Toy robot','Recursion','Word processing','Accounting App','Fix the bugs']
+
+
+topics = {
+    'The Basics': ['Hangman 1'],
+    'Making Decisions' : []
+}
+
+
+def config(username):
+    os.system(f'echo "---\neditor: code\nrepo_path: ~/problems\nnavigator_url: "https://navigator.wethinkcode.co.za"\nusername: {username}@student.wethinkcode.co.za\nreview_manager_url: "https://review-manager.wethinkcode.co.za"\nkeycloak_url: "https://keycloak.wethinkcode.co.za"" > ~/.config/wtc/config.yml')
+    """
+    ---
+    editor: code
+    repo_path: ~/problems
+    navigator_url: "https://navigator.wethinkcode.co.za"
+    username: ndumasi@student.wethinkcode.co.za
+    review_manager_url: "https://review-manager.wethinkcode.co.za"
+    keycloak_url: "https://keycloak.wethinkcode.co.za"
+    """
+        
+    
+    
+    
+def install_selenium():
+    value = subprocess.check_output("pip3 install selenium", shell=True)
+    if b'Requirement already satisfied: selenium':
+        print('Selenium already installed...\nImporting selenium...')
+    import selenium
 
 
 def check_output_login():
@@ -15,7 +43,7 @@ def check_output_login():
         value = subprocess.check_output("wtc-lms login", shell=True)
     except subprocess.CalledProcessError as e:
         print("Authentication Failed! Please try again")
-        value = check_output()
+        value = check_output_login()
     return value
 
 
@@ -27,14 +55,34 @@ def register():
     os.system("""wtc-lms register""")
 
 
+def verify_user(username):
+    os.system('wtc-lms config')
+    #subprocess.check_output('wtc-lms config', shell=True)
+    if username not in subprocess.getoutput('wtc-lms config')[190:]:
+        print('changing config file...')
+        time.sleep(1)
+        janet()
+        if username not in subprocess.getoutput('wtc-lms config')[190:]:
+            print("Invalid user\n")
+            username = get_username()
+            
+    return username
+
+
 def get_username():
-    username = input("Username: ")
+    username = input("Username: ").lower()
+    
+    # print(subprocess.getoutput('wtc-lms config')[190:].find(username))
+    
     clear()
+    username = verify_user(username)
+    
     return username
 
 
 def initializing():
     username = get_username()
+    # install_selenium()
     if os.path.exists(f"{os.environ['HOME']}/Downloads/wtc-lms"):
         print(f"Hello {username}\n")
 
@@ -43,54 +91,159 @@ def initializing():
             clear()
             print("SUCCESSFUL LOGIN\n")
             register()
+    return username
 
 
 def help_():
-    for i in list_of_commands:
-        print(i)
+    print("""Here is what is available under LMS:\n""")
+    for i in lms_commands:
+        print(f' > {i.upper()}')
+    print()
+    
+    print("Here are interface commands:\n")
+    commands()
 
 
 def commands():
     for i in list_of_commands:
-        print(i)
+        print(f' > {i.upper()}')
     print()
 
 
-def home():
-    os.system('cd')
-    
-    
 def pwd():
     return os.system('pwd')
-    
-    
+
+
 def get_command_():
-    return input(f' > {pwd()}: ')
+    return input(f' > : ')
+
+
+def janet():
+    os.system('mv ~/.config/wtc/config.yml ~/.config/wtc/config.ym0; mv ~/.config/wtc/config.ym ~/.config/wtc/config.yml; mv ~/.config/wtc/config.ym0 ~/.config/wtc/config.ym')
+
+
+def fundamentals(command,module):
+    if command in module:
+        fundamentals = module.split()[-1]
+        print(f'fundamentals uuid {fundamentals}')
+        return fundamentals
+
+
+def object_(command,module):
+    if command in module:
+        object_ = module.split()[-1]
+        print(f'Object Orientation uuid {object_}')
+        return object_
+
+
+def web(command,module):
+    # print(module,'\n')
+    if command in module:
+        web = module.split()[-1]
+        print(f'Web Application uuid {web}')
+        return web
+
+
+def mobile(command,module):
+
+    if command in module:
+        mobile = module.split()[-1]
+        print(f'Mobile applications uuid {mobile}')
+        return mobile
+    
+    
+def distributed(command,module):
+
+    if command in module:
+        distributed = module.split()[-1]
+        print(f'Distributed systems uuid {distributed}')
+        return distributed
+
+
+def lms_modules_topics(command):
+    command = command.lower()
+
+    modules = subprocess.getoutput('wtc-lms modules').splitlines()
+
+    # i = [fundamentals(command,module).strip('()') for module in modules  if 'fundamentals' in command and 'fundamentals' in module]
+    # return i
+    for module in modules:
+        module = module.lower()
+        if 'fundamentals' in command and 'fundamentals' in module:
+            uuid = fundamentals(command,module)
+            return uuid.strip('()')
+
+
+        if 'object' in command and 'object' in module :
+            uuid = object_(command,module)
+            return uuid.strip('()')
+        
+        if 'web' in command and 'web' in module:
+            uuid = web(command,module)
+            return uuid.strip('()')
+
+        if 'mobile' in command and 'mobile' in module:
+            uuid = mobile(command,module)
+            return uuid.strip('()')
+
+        if 'distr' in command and 'distri' in module:
+            uuid = distributed(command,module)
+            return uuid.strip('()')
+
+
+
+# def terminal():
+#     print("Welcome to terminal functionality...Feel asfree as the terminal")
+#     while True:
+#         cmd = str(input()
+#         os.system(str(input("")))
+    
+    
+    
 def main():
 
-    
-    initializing()
+    username = initializing()
     clear()
     print('Welcome to the interface...\n')
-    time.sleep(2)
+    time.sleep(1)
     
     
     
+    fun_topic = input('Which TOPIC are you working on...: ')
+    problem = input('What PROBLEM are you working on...: ')
+    
+    for i in f_flags:
+        if fun_topic.lower() in i:
+            uuid = lms_modules_topics(fun_topic)
+            os.system(f"wtc-lms topics {uuid}")
+            # subprocess.getoutput(f"wtc-lms topics {uuid}")
     while True:
         #print(f' > {pwd()}')
+
+
+
         command = get_command_()
-        if command.lower() in list_of_commands:
-            if 'help' in command:
+        if command.lower() in list_of_commands or command.lower() in f_flags:
+            if 'help' in command.lower():
                 help_()
             elif command.lower() == 'off':
                 break
             
-            elif command.lower() == 'home':
-                home()
-            
-            
+
         else:
             print(f"Command '{command}' does not exist")
-            command = get_command_()
+    clear()
+    print(f"Enjoy your day {username}")
+    time.sleep(2)
 
-main()
+
+
+
+
+config('username')
+# main()
+# help_()
+# terminal()
+# print(lms_modules_topics('object'))
+# get_username()
+# print(topics['learning with python'])
