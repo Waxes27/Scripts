fbaa0754dced1e9d2e763bd7b756432522ffa02e
c5r9s2 <c5r9s2@c5r9s2.co.za>
2020-11-08 11:17:28 +0200

    updated...

--- Python/bashrc_to_python/setup.py
+++ Python/bashrc_to_python/setup.py
@@ -1,6 +1,9 @@
 import subprocess
 import sys
 import os
+import time
+
+
 bashrc = open(f"{os.environ['HOME']}/.bashrc", 'r+')
 
 
@@ -10,6 +13,14 @@ if subprocess.getoutput(('echo $SHELL')) != "/bin/bash":
 
 if not os.path.exists(f"{os.environ['HOME']}/Scripts"):
     a = subprocess.getoutput('git clone https://github.com/Waxes27/Scripts.git')
+    # print(a)
+else:
+    os.system('rm -rf ~/Scripts')
+    # print("REMVOED SCRIPTS FOLDER")
+    os.system('python3 setup.py')
 
 if not "python3 ~/Scripts/Python/bashrc_to_python/lms_login.py\n" in bashrc.read() and os.path.exists(f"{os.environ['HOME']}/Scripts"):
     bashrc.write("python3 ~/Scripts/Python/bashrc_to_python/lms_login.py\n")
+
+os.system("clear")
+print("SETUP COMPLETED SUCCESSFULLY\n\n")
