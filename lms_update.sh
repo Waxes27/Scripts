c370590d49cd71754ef99c4aeb26f9ebaaba5725
Waxes27 <nceba.dumasi@gmail.com>
2021-03-15 20:46:18 +0200

    Lms Update works

--- lms_update.sh
+++ lms_update.sh
@@ -1,1 +1,15 @@
-wget
+# source lms_update.sh
+clear
+# exit
+echo "Downloading LMS..."
+
+wget www.georgepauer.com/lms/wtc-lms
+clear
+
+chmod +x wtc-lms
+sudo mv wtc-lms /bin
+
+clear
+echo "LMS UPDATED...\nPlease restart Terminal"
+sleep 2
+# exit
