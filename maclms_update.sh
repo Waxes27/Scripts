070ecca3be7533d33a67848652e54c5a7ffa7e94
waxes27 <nceba.dumasi@gmail.com>
2021-03-16 17:42:46 +0200

    update

--- maclms_update.sh
+++ maclms_update.sh
@@ -5,12 +5,11 @@ echo "Downloading LMS..."
 
 wget www.georgepauer.com/maclms/macos_wtc-lms
 mv macos_wtc-lms wtc-lms
-clear
 
 chmod +x wtc-lms
 sudo mv wtc-lms /bin
 
-clear
+
 echo "LMS UPDATED...\nPlease restart Terminal"
 sleep 2
 # exit
