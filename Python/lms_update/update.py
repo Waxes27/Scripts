095549c9822824a9be352775212dd31f30ae947c
Waxes27 <nceba.dumasi@gmail.com>
2021-03-18 13:42:55 +0200

    update

--- Python/lms_update/update.py
+++ Python/lms_update/update.py
@@ -1,11 +1,25 @@
+import os
 import requests
 
+token = "xapp-1-A01RVFHU5HS-1866587982598-00489a85d3ab40869088e98724548d51a08c3f392d25a1c84f1cfe115d276ce7"
+file_id = "F12345678"
 
-drive = 'https://drive.google.com/file/d/1_nqx5eEJMbGXwem-Jl2_qcqr5VYkOYEu/view'
+# call file info to get url
+url = "https://slack.com/api/files.info"
+r = requests.get(url, {"token": token, "file": file_id})
+r.raise_for_status
+response = r.json()
+assert response["ok"]
+file_name = response["file"]["name"]
+file_url = response["file"]["url_private"]
+print("Downloaded " + file_name)
 
-stuff = requests.get(drive).content
+# download file
+r = requests.get(file_url, headers={'Authorization': 'Bearer %s' % token})
+r.raise_for_status
+file_data = r.content   # get binary content
 
-for i in stuff.splitlines():
-    if b'href' in i:
-        print(i)
-        print()
+# save file to disk
+with open(file_name , 'w+b') as f:
+  f.write(bytearray(file_data))
+print("Saved " + file_name + " in current folder")
