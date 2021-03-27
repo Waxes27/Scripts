77b2faff63e0ebd0ad7ba576eb087d79abc2a343
Waxes27 <nceba.dumasi@gmail.com>
2021-03-27 22:50:38 +0200

    Major update

--- Python/bashrc_to_python/globals_.py
+++
@@ -1,4 +0,0 @@
-list_of_commands = ['help', 'off', 'terminal (in dev)','problems','topic']
-lms_commands = ['Topic','Problems','Reviews','Modules']
-f_flags = ['fundamentals', 'object orientation', 'web application', 'Mobile Applications', 'Distributed Systems']
-p_flags = ['Hangman','Pyramid','Outline','Mastermind','Toy robot','Recursion','Word processing','Accounting App','Fix the bugs']
